1bf27357beea814211b6b7fb8ed77c56e5460157
VictorKingsHub <ezevictornkemjika@gmail.com>
2024-03-09 21:46:55 -0800

    deployed

--- src/conponents/Home.jsx
+++ src/conponents/Home.jsx
@@ -1,49 +1,58 @@
 import React, { useEffect, useState } from 'react'
+import UserCard from './UserCard';
+import "./styles.css"
 
 const Home = () => {
-    const [userName, setUserName] = useState("")
-    const [userDate, setUserData] = useState(null)
+    const [userName, setUserName] = useState("VictorKingsHub");
+    const [userData, setUserData] = useState(null)
     const [loading, setLoading] = useState(false)
 
-    async function fetchGithubData(){
+    async function fetchGithubUserData() {
         setLoading(true)
         const response = await fetch(`https://api.github.com/users/${userName}`);
+
         const data = await response.json()
 
         try {
-            if(data){
+            if (data) {
                 setUserData(data)
                 setLoading(false)
+                setUserName("")
             }
         } catch (error) {
-            setLoading(false)
-            return <div>Error Occured, {error}</div>
+            return <div>Something went wrong {error.message}</div>
         }
 
-        if(loading){
-            return <div>Loading Data, Please, wait!!!</div>
-        }
-
-        console.log(data)
     }
 
-    const HandleSubmit = () => {}
+    const handleSubmit = () => { 
+        fetchGithubUserData()
+    }
 
-    useEffect(()=>{
-        fetchGithubData()
-    },[userDate])
+    useEffect(() => {
+        fetchGithubUserData()
+    }, [])
+
+    if (loading) {
+        return <h3>Please, wait Data is Loading...</h3>
+    }
 
     return (
-        <div className='github-search-wrapper'>
-            <div className="search-container">
+        <div className='github-profile-wrapper'>
+            <div className="input-container">
                 <input
                     type="text"
-                    name="search-github-name"
+                    name="input-name"
                     placeholder='Enter Github Name'
                     value={userName}
                     onChange={(event) => setUserName(event.target.value)}
-                    />
-                    <button className="submit" onSubmit={HandleSubmit} >Submit</button>
+                />
+                <button className="submit" onClick={handleSubmit} >Submit</button>
+            </div>
+            <div className="card">
+                {userData !== null
+                    ? <UserCard user={userData}/>
+                    : <div> Username doesn't exist</div>}
             </div>
 
         </div>
---
+++ src/conponents/UserCard.jsx
@@ -0,0 +1,39 @@
+import "./styles.css"
+function UserCard({ user }) {
+
+    const { avatar_url, followers, following, public_repos, login, url, created_at } = user;
+
+    const createdDate = new Date(created_at)
+    return (
+        <div className="user-card">
+            <div className="avatar">
+                <img src={avatar_url} alt="" />
+            </div>
+            <div className="details">
+
+            <a href={`https://github.com/${login}`}>{login} </a>
+            <p>
+                User joined github on {`${createdDate.getDate( )} ${createdDate.toLocaleString("en-us" , {
+                    month: "short"
+                })}  ${createdDate.getFullYear()}`}
+            </p>
+            <div>
+                <div>
+                    <p>Public repo</p>
+                    {public_repos}
+                </div>
+                <div>
+                    <p>Following</p>
+                    {following}
+                </div>
+                <div>
+                    <p>Followers</p>
+                    {followers}
+                </div>
+            </div>
+                </div>
+        </div>
+    )
+}
+
+export default UserCard
