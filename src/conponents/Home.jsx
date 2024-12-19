ebe5a7e013ce2da7a38653a5a229d4e6caf3a4d6
VictorKingsHub <ezevictornkemjika@gmail.com>
2024-12-19 01:28:42 +0100

    re-styled

--- src/conponents/Home.jsx
+++ src/conponents/Home.jsx
@@ -1,73 +1,77 @@
 import React, { useEffect, useState } from 'react'
-import UserCard from './UserCard';
-import "./styles.css"
+import UserCard from './UserCard'
+import './styles.css'
 
 const Home = () => {
-    const [userName, setUserName] = useState("VictorKingsHub");
-    const [userData, setUserData] = useState(null)
-    const [loading, setLoading] = useState(false)
-    const [failedFetch, setFailedFetch] = useState(false)
+  const [userName, setUserName] = useState('VictorKingsHub')
+  const [userData, setUserData] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [failedFetch, setFailedFetch] = useState(false)
 
-    async function fetchGithubUserData() {
-        setLoading(true)
-        setFailedFetch(false)
-        const response = await fetch(`https://api.github.com/users/${userName}`);
+  async function fetchGithubUserData () {
+    setLoading(true)
+    setFailedFetch(false)
+    const response = await fetch(`https://api.github.com/users/${userName}`)
 
-        const data = await response.json()
+    const data = await response.json()
 
-        try {
-            if (data) {
-                setUserData(data)
-                setLoading(false)
-                setUserName("")
-                setFailedFetch(false)
-            }
-        } catch (error) {
-            return <div>Something went wrong {error.message}</div>
-        }
-
-    }
-
-    const handleSubmit = () => { 
-        fetchGithubUserData()
-    }
-
-    useEffect(() => {
-        fetchGithubUserData()
-        .catch(err => {
-            setLoading(false)
-            console.log(err.message)
-        })
-    }, [])
-
-    if (loading) {
-        return <h3>Please, wait Data is Loading...</h3>
-    }
-    if (failedFetch) {
+    try {
+      if (data) {
+        setUserData(data)
         setLoading(false)
-        return <h3 style={{color: "yellow"}}>Username doesn't exist</h3>
+        setUserName('')
+        setFailedFetch(false)
+      }
+    } catch (error) {
+      return <div>Something went wrong {error.message}</div>
     }
+  }
 
-    return (
-        <div className='github-profile-wrapper'>
-            <div className="input-container">
-                <input
-                    type="text"
-                    name="input-name"
-                    placeholder='Enter Github Name'
-                    value={userName}
-                    onChange={(event) => setUserName(event.target.value)}
-                />
-                <button className="submit" onClick={handleSubmit} >Submit</button>
-            </div>
-            <div className="card">
-                {userData !== null
-                    ? <UserCard user={userData}/>
-                    : <div> Username doesn't exist</div>}
-            </div>
+  const handleSubmit = () => {
+    fetchGithubUserData()
+  }
 
+  useEffect(() => {
+    fetchGithubUserData().catch(err => {
+      setLoading(false)
+      console.log(err.message)
+    })
+  }, [])
+
+  if (loading) {
+    return <h3>Please, wait Data is Loading...</h3>
+  }
+  if (failedFetch) {
+    setLoading(false)
+    return <h3 style={{ color: 'yellow' }}>Username doesn't exist</h3>
+  }
+
+  return (
+    <div className='github-profile-wrapper'>
+      <div className='input-container'>
+          <span className='title'>Github Profile Finder</span>
+        <div className='up'>
+          <input
+            type='text'
+            name='input-name'
+            placeholder='Enter Github Name'
+            value={userName}
+            onChange={event => setUserName(event.target.value)}
+          />
+        <button className='submit' onClick={handleSubmit}>
+          Submit
+        </button>
         </div>
-    )
+      </div>
+      <div className='card'>
+        {userData !== null ? (
+          <UserCard user={userData} />
+        ) : (
+          <div> Username doesn't exist</div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Home
--- src/conponents/UserCard.jsx
+++ src/conponents/UserCard.jsx
@@ -1,39 +1,50 @@
-import "./styles.css"
-function UserCard({ user }) {
+import './styles.css'
+function UserCard ({ user }) {
+  const {
+    avatar_url,
+    followers,
+    following,
+    public_repos,
+    login,
+    url,
+    created_at
+  } = user
 
-    const { avatar_url, followers, following, public_repos, login, url, created_at } = user;
-
-    const createdDate = new Date(created_at)
-    return (
-        <div className="user-card">
-            <div className="avatar">
-                <img src={avatar_url} alt="" />
+  const createdDate = new Date(created_at)
+  return (
+    <div className='user-card'>
+      <div className='avatar'>
+        <img src={avatar_url} alt='' />
+      </div>
+      <div className='details'>
+        <div className='text'>
+          <a className='name' href={`https://github.com/${login}`}>
+            {login}{' '}
+          </a>
+          <div>
+            <div className='light_dark'>
+              <span>Public repo: </span>
+              {public_repos}
             </div>
-            <div className="details">
-
-            <a href={`https://github.com/${login}`}>{login} </a>
-            <div>
-                <div>
-                    <span>Public repo: </span>
-                    {public_repos}
-                </div>
-                <div>
-                    <span>Following: </span>
-                    {following}
-                </div>
-                <div>
-                    <span>Followers: </span>
-                    {followers}
-                </div>
+            <div className='light_dark'>
+              <span>Following: </span>
+              {following}
             </div>
-            <p>
-                User joined github on {`${createdDate.getDate( )} ${createdDate.toLocaleString("en-us" , {
-                    month: "short"
-                })}  ${createdDate.getFullYear()}`}
-            </p>
-                </div>
+            <div className='light_dark'>
+              <span>Followers: </span>
+              {followers}
+            </div>
+          </div>
+          <p className='joined'>
+            User joined github on{' '}
+            {`${createdDate.getDate()} ${createdDate.toLocaleString('en-us', {
+              month: 'short'
+            })}  ${createdDate.getFullYear()}`}
+          </p>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default UserCard
